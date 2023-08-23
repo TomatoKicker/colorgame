61dacf307eda0e5d511827f020f8a90b0b13ca5b
TomatoKicker <phi465@live.de>
2023-08-23 22:32:26 +0200

    slowed down lightening of color

--- src/components/ColorUnit.jsx
+++ src/components/ColorUnit.jsx
@@ -20,7 +20,7 @@ const ColorUnit = ({ color, odd, id, increment, round, showModal, score }) => {
         return newHexColor;
     }
 
-    let lighten = 30 - round * 1;
+    let lighten = 30 - (round >= 29 ? 29 : round * 1);
     const style = {
         backgroundColor: odd === "1" ? lightenHexColor(color, lighten) : color
     }
